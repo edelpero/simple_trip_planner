3dcba521244bd96d30273595cb5ed20987faf8ba
Ezequiel Delpero <edelpero@gmail.com>
2014-05-31 10:49:44 -0300

    Adds Trip model spec

---
+++ spec/models/trip_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Trip, :type => :model do
+  describe 'validations' do
+    it { should validate_presence_of(:destination) }
+    it { should validate_presence_of(:start_date) }
+    it { should validate_presence_of(:end_date) }
+
+    it 'validates end date to be greater than start date' do
+      invalid_trip = FactoryGirl.build(:trip, start_date: '2014-05-28', end_date: '2014-05-27')
+      invalid_trip.valid?
+      expect(invalid_trip.errors).to include(:start_date)
+    end
+  end
+end
