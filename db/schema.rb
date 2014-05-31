6bffc37a658420ca7f0e4e056d35a11d0ef3480f
Ezequiel Delpero <edelpero@gmail.com>
2014-05-31 10:51:11 -0300

    Associates User model with Trip model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140528233037) do
+ActiveRecord::Schema.define(version: 20140528234042) do
+
+  create_table "trips", force: true do |t|
+    t.integer  "user_id"
+    t.string   "destination"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.text     "comment"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "trips", ["user_id"], name: "index_trips_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
