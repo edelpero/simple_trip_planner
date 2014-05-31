8cb9802ce81649775efbaa1250c2821ffe2bb75b
Ezequiel Delpero <edelpero@gmail.com>
2014-05-31 11:53:59 -0300

    Updates Trip factory's start_date and end_date

--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -2,8 +2,8 @@ FactoryGirl.define do
   factory :trip do
     user
     destination { Faker::Venue.name }
-    start_date  "2014-05-12"
-    end_date    "2014-05-28"
+    start_date  { Date.current }
+    end_date    { 12.days.from_now }
     comment     { Faker::Lorem.paragraph(4) }
   end
 end
