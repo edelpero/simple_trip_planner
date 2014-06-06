cf33ea87e241d457db5005f2ff569839d18cf4cf
Ezequiel Delpero <edelpero@gmail.com>
2014-06-05 23:06:44 -0300

    Updates user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe User, :type => :model do
+  describe 'associations' do
+    it { should have_many(:trips) }
+  end
+
   describe 'validations' do
     it { should validate_presence_of(:username) }
     it { should validate_presence_of(:name) }
