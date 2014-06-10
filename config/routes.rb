d3fbdeeb4270038b0ccdc15591d853ea0405e76b
Ezequiel Delpero <edelpero@gmail.com>
2014-06-09 23:08:17 -0300

    Adds /trips/next_month url to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :trips
+  resources :trips do
+    collection do
+      get :next_month
+    end
+  end
 
   root 'trips#index'
 end
