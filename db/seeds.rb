4ab48fbec79fb1d02ff930ad0680fd4f82fce710
Ezequiel Delpero <edelpero@gmail.com>
2014-06-10 09:10:54 -0300

    Adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'ffaker'
+
+beginning_date = 3.weeks.ago.to_date
+
+user = User.create(email: 'admin@simpletripplanner.com', password: 'thisisatest',
+  password_confirmation: 'thisisatest')
+
+40.times {
+  end_date = beginning_date + 3.days
+
+  trip = Trip.new(
+    destination: Faker::Address.country,
+    start_date:  beginning_date,
+    end_date:    end_date,
+    user:        user)
+  trip.save(validate: false)
+
+  beginning_date = end_date + 1.day
+}
